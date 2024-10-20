c8cf98cb64064adbbf0d1e813cb2a185f7263041
3ovospor1real <joaohumb2007@gmail.com>
2024-10-20 09:44:43 -0300

    Aula 36

---
+++ aula36/main.js
@@ -0,0 +1,18 @@
+const fruits = ['Apple', 'Banana', 'Grape'];
+
+for(i in fruits) {
+    console.log(i, fruits[i]);
+}
+
+console.log('///////////////////////////////////////////////////////////////////////////////////////////')
+
+const person = {
+    name: 'João',
+    surname: 'Ribeiro',
+    age: 16,
+}
+
+for(i in person) {
+    console.log(i, person[i]);
+}
+
